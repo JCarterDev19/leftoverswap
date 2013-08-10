33fd83576538dbe647b9bd8bf7b1ca5cabe58b64
dnewms <dnewms@hemingway.hsd1.wa.comcast.net>
2013-08-09 19:36:46 -0700

    new welcome screen! logo still jumps from launch to app.

--- LeftoverSwap/LSWelcomeViewController.m
+++ LeftoverSwap/LSWelcomeViewController.m
@@ -18,50 +18,50 @@
 
 - (void)viewDidLoad {
   self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"welcome"]];
-  
+
   NSInteger screenWidth = [UIScreen mainScreen].bounds.size.width;
 
-//  {
-//    NSString *text = @"LeftoverSwap";
-//    CGSize textSize = [text sizeWithFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:36.0f]];
-//    UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectMake( (screenWidth - textSize.width)/2.0f, 200.0f, textSize.width, textSize.height)];
-//    [textLabel setFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:36.0f]];
-//    [textLabel setText:text];
-//    [textLabel setTextColor:[UIColor blackColor]];
-//    [textLabel setBackgroundColor:[UIColor clearColor]];
-//    [textLabel setTextAlignment:UITextAlignmentCenter];
-//    
-//    [self.view addSubview:textLabel];
-//  }
+ {
+   NSString *text = @"LeftoverSwap";
+   CGSize textSize = [text sizeWithFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:36.0f]];
+   UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectMake( (screenWidth - textSize.width)/2.0f, 180.0f, textSize.width, textSize.height)];
+ [textLabel setFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:36.0f]];
+   [textLabel setText:text];
+   [textLabel setTextColor:[UIColor blackColor]];
+   [textLabel setBackgroundColor:[UIColor clearColor]];
+   [textLabel setTextAlignment:UITextAlignmentCenter];
+   
+   [self.view addSubview:textLabel];
+ }
 
   {
-    NSString *text = @"Eat";
+    NSString *text = @"eat";
     CGSize dimens = CGSizeMake(240, 80);
     
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
     [button setFrame:CGRectMake((screenWidth - dimens.width)/2.0f, 260.0f, dimens.width, dimens.height)];
     [button setTitle:text forState:UIControlStateNormal];
-    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [[button titleLabel] setFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:28.0f]];
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [[button titleLabel] setFont:[UIFont fontWithName:@"HelveticaNeue-Bold" size:28.0f]];
     [button setTitleEdgeInsets:UIEdgeInsetsMake( 0.0f, 5.0f, 0.0f, 0.0f)];
     [button addTarget:self action:@selector(eatButtonAction:) forControlEvents:UIControlEventTouchUpInside];
-    [button setBackgroundColor:[UIColor whiteColor]];
+    [button setBackgroundColor:[UIColor colorWithRed:(19.0/255.0f) green:(128.0/255.0f) blue:(8.0/255.0f) alpha:1.0f]];
     
     [self.view addSubview:button];
   }
 
   {
-    NSString *text = @"Feed";
+    NSString *text = @"feed";
     CGSize dimens = CGSizeMake(240, 80);
 
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
     [button setFrame:CGRectMake((screenWidth - dimens.width)/2.0f, 360.0f, dimens.width, dimens.height)];
     [button setTitle:text forState:UIControlStateNormal];
-    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [[button titleLabel] setFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:28.0f]];
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+      [[button titleLabel] setFont:[UIFont fontWithName:@"HelveticaNeue-Bold" size:28.0f]];
     [button setTitleEdgeInsets:UIEdgeInsetsMake( 0.0f, 5.0f, 0.0f, 0.0f)];
     [button addTarget:self action:@selector(feedButtonAction:) forControlEvents:UIControlEventTouchUpInside];
-    [button setBackgroundColor:[UIColor whiteColor]];
+    [button setBackgroundColor:[UIColor colorWithRed:(19.0/255.0f) green:(128.0/255.0f) blue:(8.0/255.0f) alpha:1.0f]];
     
     [self.view addSubview:button];
   }
