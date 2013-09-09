f74d9ceec4390d045418eca8fb9f2c9007a4797c
Dan Newman <dnewms@gmail.com>
2013-09-08 18:46:49 -0700

    one last time trying to sync this MapView file

---
+++ LeftoverSwap/LSSingleConversationViewController.h
@@ -0,0 +1,14 @@
+//
+//  LSSingleConversationViewController.h
+//  LeftoverSwap
+//
+//  Created by Dan Newman on 9/7/13.
+//  Copyright (c) 2013 LeftoverSwap. All rights reserved.
+//
+
+#import "LSConversationViewController.h"
+#import <Parse/Parse.h>
+
+@interface LSSingleConversationViewController : LSConversationViewController
+
+@end
---
+++ LeftoverSwap/LSSingleConversationViewController.m
@@ -0,0 +1,40 @@
+//
+//  LSSingleConversationViewController.m
+//  LeftoverSwap
+//
+//  Created by Dan Newman on 9/7/13.
+//  Copyright (c) 2013 LeftoverSwap. All rights reserved.
+//
+
+#import "LSSingleConversationViewController.h"
+#import "LSConstants.h"
+
+@interface LSSingleConversationViewController ()
+
+
+@end
+
+@implementation LSSingleConversationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
