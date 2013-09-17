0df22de852b981d37cd0487d72cdd7f3e3abf486
Bryan Summersett <bsummersett@gmail.com>
2013-09-16 17:33:51 -0700

    removing hockeyapp 3.0

--- LeftoverSwap/LSAppDelegate.h
+++ LeftoverSwap/LSAppDelegate.h
@@ -10,7 +10,7 @@
 #import <CoreLocation/CoreLocation.h>
 #import "LSLocationController.h"
 
-@interface LSAppDelegate : UIResponder <UIApplicationDelegate>
+@interface LSAppDelegate : UIResponder <UIApplicationDelegate, BITHockeyManagerDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
